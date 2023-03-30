328a6a63909fbc096846a51696de7f9148f5bcf7
rizkyyanuark <rizkyyanuarkristianto@gmail.com>
2023-03-31 05:26:22 +0700

    test

--- tugas psd/test.py
+++ tugas psd/test.py
@@ -1,52 +1,163 @@
-from scipy.io.wavfile import write
-# import numpy as np
-# import matplotlib.pyplot as plt
-# data = np.genfromtxt('Data_sinyal.csv', delimiter=',')
-# data = np.int16(data / np.max(np.abs(data)) * 32767)
-# write('Data_siyal_csv_yg_diubah.wav', 48000, data)
-# fft_data = np.fft.fft(data)
-# plt.plot(np.abs(fft_data))
-# plt.show()
-
-import numpy as np
 import matplotlib.pyplot as plt
+import csv
+import numpy as np
+from scipy.io.wavfile import write
+from scipy.io import wavfile
 
-# Baca data sinyal dari file .csv
-data = np.genfromtxt('Data_sinyal.csv', delimiter=',')
+# -----mengubah file Data sinyal csv menjadi wav-----
+# Membaca file CSV
+with open('Data_sinyal.csv', 'r') as f:
+    reader = csv.reader(f)
+    data = list(reader)
 
-# Normalisasi data menjadi bilangan bulat 16-bit
-data = np.int16(data / np.max(np.abs(data)) * 32767)
+# Mengubah data ke dalam format array numpy
+data_np = np.array(data, dtype=float)
 
-# Baca data noise dari file .csv
-noise = np.genfromtxt('Data_noise.csv', delimiter=',')
+# Menentukan parameter WAV (contoh: frekuensi sampling 44100 Hz)
+fs = 48000
 
-# Normalisasi data noise menjadi bilangan bulat 16-bit
-noise = np.int16(noise / np.max(np.abs(noise)) * 32767)
+# Menyimpan data sinyal dalam format WAV
+write('UTS_Sinyal.wav', fs, data_np)
+# ----------------------------------------
 
-# Tambahkan noise pada data sinyal
-data_with_noise = np.int16(data + noise)
 
-# Transformasi FFT
-fft_data = np.fft.fft(data_with_noise)
+# ---membuat plot atau sinyal dari hasil sinyal.wav---
 
-# Hitung amplitudo spektrum frekuensi
-amplitude_spectrum = np.abs(fft_data)
+# Membaca file WAV
+fs, data = wavfile.read('UTS_sinyal.wav')
 
-# # Hitung skala frekuensi dalam Hz
-freq_scale = np.fft.fftfreq(len(data_with_noise), d=1/44100)
+# Mengubah data ke dalam format array numpy
+data_np = np.array(data, dtype=float)
 
-# Plot sinyal input dengan noise
-time_scale = np.linspace(0, len(data_with_noise)/44100, len(data_with_noise))
+# Melakukan transformasi FFT
+fft = np.fft.fft(data_np)
+
+# Menampilkan hasil transformasi FFT
+print(fft)
+
+# Membaca file WAV
+fs, data = wavfile.read('UTS_sinyal.wav')
+
+# Mengubah data ke dalam format array numpy
+data_np = np.array(data, dtype=float)
+
+# Melakukan transformasi FFT
+fft = np.fft.fft(data_np)
+
+# Menentukan skala frekuensi
+freq = np.fft.fftfreq(data_np.shape[-1], d=1/fs)
+
+# Membuat plot sinyal input
 plt.subplot(2, 1, 1)
-plt.plot(time_scale, data_with_noise)
-plt.xlabel('Waktu (detik)')
+plt.plot(data_np)
+plt.title('Sinyal Input')
+plt.xlabel('Waktu')
 plt.ylabel('Amplitudo')
 
-# Plot spektrum frekuensi
+# Membuat plot frekuensi hasil transformasi FFT
 plt.subplot(2, 1, 2)
-plt.plot(freq_scale[:len(data_with_noise)//2],
-         amplitude_spectrum[:len(data_with_noise)//2])
-plt.xlabel('Frekuensi (Hz)')
+plt.plot(freq, np.abs(fft))
+plt.title('Frekuensi Hasil Transformasi FFT')
+plt.xlabel('Frekuensi')
 plt.ylabel('Amplitudo')
-write('Data_siyal_csv_yg_diubah.wav', 48000, data)
+
+# Menampilkan plot
+plt.show()
+
+# ---------------------------------------
+
+
+# -----mengubah file Data noise csv menjadi wav-----
+
+# Membaca file CSV
+with open('Data_noise.csv', 'r') as f:
+    reader = csv.reader(f)
+    data = list(reader)
+
+# Mengubah data ke dalam format array numpy
+data_np = np.array(data, dtype=float)
+
+# Menentukan parameter WAV (contoh: frekuensi sampling 44100 Hz)
+fs = 48000
+
+# Menyimpan data sinyal dalam format WAV
+write('UTS_noise.wav', fs, data_np)
+
+# ---membuat plot atau sinyal dari hasil sinyal.wav---
+
+# Membaca file WAV
+fs, data = wavfile.read('UTS_noise.wav')
+
+# Mengubah data ke dalam format array numpy
+data_np = np.array(data, dtype=float)
+
+# Melakukan transformasi FFT
+fft = np.fft.fft(data_np)
+
+# Menampilkan hasil transformasi FFT
+print(fft)
+
+# -----------------------------------------------------
+
+
+# -----penggabungan sinyal dan nosie------
+# Baca file UTS_sinyal.wav
+rate, signal = wavfile.read('UTS_Sinyal.wav')
+
+# Baca file Data_noise.csv
+noise_data = np.genfromtxt('Data_noise.csv', delimiter=',')
+
+# Pastikan ukuran data noise sama dengan ukuran data sinyal
+assert len(noise_data) == len(signal)
+
+# Gabungkan data noise ke data sinyal
+signal_with_noise = signal.astype(float) + noise_data
+
+# Simpan data sinyal dengan noise ke file baru
+wavfile.write('UTS_Sinyal_with_noise.wav', rate,
+              signal_with_noise.astype(np.int16))
+
+# --------------------------------------------------------------
+
+
+# ------plot frekuensi hasil transformasi FFT--------
+# Membaca file WAV
+fs, data = wavfile.read('UTS_Sinyal_dengan_noise.wav')
+
+# Mengubah data ke dalam format array numpy
+data_np = np.array(data, dtype=float)
+
+# Melakukan transformasi FFT
+fft = np.fft.fft(data_np)
+
+# Menampilkan hasil transformasi FFT
+print(fft)
+
+# Membaca file WAV
+fs, data = wavfile.read('UTS_Sinyal_dengan_noise.wav')
+
+# Mengubah data ke dalam format array numpy
+data_np = np.array(data, dtype=float)
+
+# Melakukan transformasi FFT
+fft = np.fft.fft(data_np)
+
+# Menentukan skala frekuensi
+freq = np.fft.fftfreq(data_np.shape[-1], d=1/fs)
+
+# Membuat plot sinyal input
+plt.subplot(2, 1, 1)
+plt.plot(data_np)
+plt.title('Sinyal Input')
+plt.xlabel('Waktu')
+plt.ylabel('Amplitudo')
+
+# Membuat plot frekuensi hasil transformasi FFT
+plt.subplot(2, 1, 2)
+plt.plot(freq, np.abs(fft))
+plt.title('Frekuensi Hasil Transformasi FFT')
+plt.xlabel('Frekuensi')
+plt.ylabel('Amplitudo')
+
+# Menampilkan plot
 plt.show()
