b108e3f2ea936155539415bae9a2a08f8a35dc5a
rizkyyanuark <rizkyyanuarkristianto@gmail.com>
2023-04-12 23:23:11 +0700

    rapi in folder

--- CocktailSort_Kelompok 1_UTS/show_yourresults.py
+++
@@ -1,7 +0,0 @@
-import pandas
-from skimage import color
-import matplotlib.pylab as plt
-df = pandas.read_csv('PikachuCocktail.csv')
-df.drop(columns=['indeks'])
-plt.imshow(df, cmap=plt.cm.orange, vmin=0, vmax=1)
-plt.show()
